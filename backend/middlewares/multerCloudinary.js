b2c707785deae82cef844774f5b4e1cca27e44cd
NTP <pudding2912@gmail.com>
2023-11-06 03:31:29 +0700

    update api GET body > query

--- backend/middlewares/multerCloudinary.js
+++ backend/middlewares/multerCloudinary.js
@@ -2,11 +2,21 @@ import multer from "multer";
 import cloudinary from "../utils/cloudinary_config.js";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 
+// product
+// const storage = new CloudinaryStorage({
+//     cloudinary,
+//     allowedFormats: ['jpg', 'png'],
+//     params: {
+//         folder: 'shoeshop'
+//     }
+// });
+
+// test
 const storage = new CloudinaryStorage({
     cloudinary,
-    allowedFormats: ['jpg', 'png'],
+    allowedFormats: ['jpg', 'png', 'jpeg'],
     params: {
-        folder: 'shoeshop'
+        folder: 'test'
     }
 });
 
