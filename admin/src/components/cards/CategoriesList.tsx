a0a0a187837d856f7a9f0185a09cb0fd54487196
fox <phurong1@gmail.com>
2023-12-29 11:45:37 +0700

    fix ui category admin

--- admin/src/components/cards/CategoriesList.tsx
+++ admin/src/components/cards/CategoriesList.tsx
@@ -16,33 +16,35 @@ const CategoriesList = ({ categories, setItem }: Props) => {
         });
     };
     return (
-        <div className="flex gap-5">
-            {categories &&
-                categories.map((item) => (
-                    <div
-                        key={item._id}
-                        className="flex gap-[10px] p-[10px] shadow-revenue w-[280px] bg-white cursor-pointer hover:border-2 hover:border-blue"
-                        onClick={() => handleSet(item)}
-                    >
-                        <Image
-                            src={item.image}
-                            alt="Cate"
-                            width={100}
-                            height={100}
-                            className="w-[100px] h-[100px] shadow-cate"
-                        />
-                        <div className="flex flex-col w-[150px] items-center">
-                            <div className="text-right w-full">
-                                <input type="checkbox" className="w-5 h-5" />
-                            </div>
-                            <span className="font-bold">{item.name}</span>
-                            <div className="flex justify-between mt-[30px] font-medium text-sm w-full">
-                                <span>Total:</span>
-                                <span>23</span>
+        <div className="overflow-x-scroll w-[80vw]">
+            <div className="flex gap-5">
+                {categories &&
+                    categories.map((item) => (
+                        <div
+                            key={item._id}
+                            className="flex gap-[10px] p-[10px] shadow-revenue min-w-[280px] bg-white cursor-pointer hover:border-2 hover:border-blue"
+                            onClick={() => handleSet(item)}
+                        >
+                            <Image
+                                src={item.image}
+                                alt="Cate"
+                                width={100}
+                                height={100}
+                                className="w-[100px] h-[100px] shadow-cate"
+                            />
+                            <div className="flex flex-col w-[150px] items-center">
+                                <div className="text-right w-full">
+                                    <input type="checkbox" className="w-5 h-5" />
+                                </div>
+                                <span className="font-bold">{item.name}</span>
+                                <div className="flex justify-between mt-[30px] font-medium text-sm w-full">
+                                    <span>Total:</span>
+                                    <span>23</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+            </div>
         </div>
     );
 };
